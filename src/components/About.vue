d4465e0dcb4ff3e22be29892185e03bc3c9d6c0d
Your Name <you@example.com>
2022-08-21 00:26:20 +0530

    route change

---
+++ src/components/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <label>About</label>
+    <select  v-model="$i18n.locale" class="nav__lang-switcher">
+  <option v-for="(lang, i) in ['en','in']" :key="`lang${i}`"
+   :value="lang">{{lang}}</option>
+</select>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'HelloWorld',
+  setup() {
+    const store = useStore();
+    const count = computed(() => store.state.count);
+    return {
+      count,
+      store,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,9 @@
 import HelloWorld from '@/components/HelloWorld.vue';
+import About from '@/components/About.vue';
 
 const routes = [
   { path: '/', component: HelloWorld },
-  { path: '/HelloWorld', component: HelloWorld },
+  { path: '/About', component: About },
 ];
 
 export default routes;
